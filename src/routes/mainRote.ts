67b75a304a58acb0acac8e7b65d024db4f058ad5
Ram chand <ramchand.k@ril.com>
2026-07-18 21:56:50 +0530

    teacher microservice initialization

--- src/routes/mainRote.ts
+++ src/routes/mainRote.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
-import studentRoutes from "./studentRoutes";
 import teacherRoutes from "./teacherRoutes";
-import authRoutes from "./authRoutes";
 const router = Router();
 
-router.use('/students', studentRoutes);
 router.use('/teachers', teacherRoutes);
-router.use('/auth', authRoutes);
 export default router;
