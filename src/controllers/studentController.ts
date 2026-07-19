ae1dad016c0b9d93136e0121a3d9978eb79d6e6c
Ram chand <ramchand.k@ril.com>
2026-07-19 12:00:37 +0530

    student api implemneted

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,12 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import Student, { IStudent } from '../models/Student';
 import { successResponse, errorResponse } from '../utils/messages';
-
+import logger from '../utils/logger';
 export const createStudent = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const student = await Student.create(req.body as Partial<IStudent>);
-    res.status(201).json(successResponse('Student created', student));
-  } catch (err) {
+    const result = req.body;
+    if(!result){
+      res.status(400).json(errorResponse("all fields required.."))
+    }
+
+    const Existing_student = await Student.findOne({
+      student_mail:result?.student_mail
+    })
+    if(Existing_student){
+          return res.status(409).json(errorResponse('Student already exist..'));
+
+    }
+    const student = await Student.create(result);
+    if(!student){
+          return  res.status(400).json(errorResponse("student is not created"))
+
+    }
+    else{
+    return res.status(201).json(successResponse('Student created', student));
+
+    }
+  } catch (err:any) {
+    logger.error("create student failed",{
+      error:err?.message
+    })
     next(err);
   }
 };
@@ -15,10 +37,14 @@ export const getStudents = async (req: Request, res: Response, next: NextFunctio
   try {
       
     const students = await Student.find();
+    const count_result = await Student.aggregate([{
+      $count:"students_count"
+    }])
+
     if(students.length === 0) {
       return res.json(successResponse("No students found", []));
     }else{
-      return res.json(successResponse("Students retrieved successfully", students));
+      return res.json(successResponse("Students retrieved successfully", {students, students_count:count_result[0].students_count}));
     }
   } catch (err) {
     next(err);
