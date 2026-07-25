a0233199a8fd380e8b5d3aa66703fe007655e53b
Ram chand <ramchand.k@ril.com>
2026-07-25 22:00:24 +0530

    Uncaught Exception handling...

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import config from '../webconfig.json'
 import cors from "cors"
 import { routes } from "./routes/routes"
 import helmet from "helmet"
-
+import logger from "./utils/messages/logger"
 const app = express()
 const port = 4000
 const base_url = config?.base_url;
@@ -12,6 +12,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet())
 app.use(base_url, routes);
+process.on("uncaughtException",(err)=>{
+    logger.error("Uncaught Exception", {
+        message: err.message,
+        stack: err.stack
+    });
+
+    process.exit(1);
+})
 app.listen(port, async()=>{
     console.log(`gateway is running in ${port}`)
 })  
