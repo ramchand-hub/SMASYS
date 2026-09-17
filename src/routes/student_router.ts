e10823ce82f5925039bdaf032fde3e67899af987
Ramchand <rareact007@gmail.com>
2026-09-17 17:24:43 +0530

    students routes also adde the verify token...

--- src/routes/student_router.ts
+++ src/routes/student_router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import student_controller from "../controllers/student_controller"
+import { verifyToken } from "../utils/messages/verify_token";
 const router = Router();
 
-router.post("/createStudent", student_controller)
-router.get("/getStudentsList", student_controller)
+router.post("/createStudent", verifyToken, student_controller)
+router.get("/getStudentsList", verifyToken, student_controller)
 
 export default router
