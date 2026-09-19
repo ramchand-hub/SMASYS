53302feee6f7d9ff160c94c7cee7f4aafdeb352a
Ramchand <rareact007@gmail.com>
2026-09-19 22:12:34 +0530

    promatheus client and metrics implemented

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ import logger from "./utils/messages/logger"
 const app = express()
 const port = 4000
 const base_url = config?.base_url;
+const client = require("prom-client")
+client.collectDefaultMetrics();
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +23,31 @@ process.on("uncaughtException",(err)=>{
 
     process.exit(1);
 })
+
+// Custom HTTP request counter
+const httpRequestsTotal = new client.Counter({
+  name: "gateway_http_requests_total",
+  help: "Total HTTP requests received by gateway",
+  labelNames: ["method", "route", "status_code"],
+});
+
+// Metrics endpoint
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequestsTotal.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+});
 app.listen(port, async()=>{
     console.log(`gateway is running in ${port}`)
 })  
