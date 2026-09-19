d3d02026732c3d21694a4fffdc90bb8f223e9e2a
Ramchand <rareact007@gmail.com>
2026-09-19 22:13:40 +0530

    metrics implemented for students

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,25 @@ import redis_connect from "./config/redis";
 // import "./streams/Readble_stream"
 const app = express();
 const PORT = process.env.PORT || 3002;
+const client = require("prom-client")
+client.collectDefaultMetrics();
+const httpRequestsTotal = new client.Counter({
+  name: "student_service_http_requests_total",
+  help: "Total HTTP requests received by student service",
+  labelNames: ["method", "route", "status_code"],
+});
 
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequestsTotal.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+});
 app.use(morgan("dev"));
 app.use(cors({
   origin: "*",
@@ -18,6 +36,10 @@ app.use(cors({
 
 app.use(express.json());
 app.use("/", mainRouter);
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
 app.use((req, res) => {
   res.status(404).json({
     status: 404,
@@ -25,7 +47,7 @@ app.use((req, res) => {
   });
 });
 
-app.use(errorHandler);  
+app.use(errorHandler);
 app.listen(PORT, async () => {
   await connectDB();
   // const redisclient =  redis_connect();
