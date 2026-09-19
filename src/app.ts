14ccd01e188e959ba566013b8b7633c6ca3d654d
Ramchand <rareact007@gmail.com>
2026-09-19 22:11:34 +0530

    metrics added for authentication

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,25 @@ import redis_connect from "./config/redis";
 // import "./streams/Readble_stream"
 const app = express();
 const PORT = process.env.PORT || 3001;
+const client = require("prom-client");
+client.collectDefaultMetrics();
+const httpRequestsTotal = new client.Counter({
+  name: "student_service_http_requests_total",
+  help: "Total HTTP requests received by student service",
+  labelNames: ["method", "route", "status_code"],
+});
+
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequestsTotal.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+});
 
 app.use(morgan("dev"));
 app.use(cors({
@@ -25,7 +44,11 @@ app.use((req, res) => {
   });
 });
 
-app.use(errorHandler);  
+app.use(errorHandler);
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
 app.listen(PORT, async () => {
   await connectDB();
   // const redisclient =  redis_connect();
