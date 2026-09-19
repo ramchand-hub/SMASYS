8d3543d645528a44a4a3aa5443bd31307a062931
Ramchand <rareact007@gmail.com>
2026-09-19 22:31:02 +0530

    validations added for register and login

--- src/routes/auth_router.ts
+++ src/routes/auth_router.ts
@@ -1,11 +1,12 @@
 import auth_controller from "../controllers/auth_controller"
 import { Router } from "express"
-
+import validate from "../middleware/validation";
+import { loginSchema, registerSchema } from "../middleware/auth.schema";
 const router = Router()
 
 
-router.post("/login",auth_controller);
-router.post("/register",auth_controller);
+router.post("/login",validate(loginSchema),auth_controller);
+router.post("/register",validate(registerSchema),auth_controller);
 router.post("/forgot-password",auth_controller);
 router.post("/update-password",auth_controller);
 router.post("/uploadfile",auth_controller);
